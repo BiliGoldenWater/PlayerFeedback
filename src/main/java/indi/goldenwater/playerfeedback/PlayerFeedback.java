1dee51ef897a23953caab87f967cdf4a6958e2c3
BiliGoldenWater <2439577268@qq.com>
2021-03-10 16:59:28 +0800

    Initial commit

---
+++ src/main/java/indi/goldenwater/playerfeedback/PlayerFeedback.java
@@ -0,0 +1,17 @@
+package indi.goldenwater.playerfeedback;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerFeedback extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
